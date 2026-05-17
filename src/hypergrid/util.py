a064d5f9cf79a2ace9f9831a2db3e608a5510459
Justin <justin@yan.family>
2026-05-17 08:18:31 -0700

    Formatting

--- src/hypergrid/util.py
+++ src/hypergrid/util.py
@@ -1,5 +1,5 @@
-from typing import Callable, Type
+from typing import Callable
 
 
-def instantiate_lambda(cls: Type) -> Callable:
+def instantiate_lambda(cls: type) -> Callable:
     return lambda ge: cls(**ge._asdict())
